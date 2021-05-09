f2b917c0498d4325c5d67ac5990e0c95d0ec15e3
bright-poku <bright.adupoku@auth0.com>
2021-05-08 22:58:31 -0400

    gofmt clean up whitespaces

--- mvc/domain/item.go
+++ mvc/domain/item.go
@@ -1,8 +1,8 @@
 package domain
 
 type Item struct {
-	Name 			uint64 `json:"name"`
-	Description 	string `json:"description"`
-	Homepage 	string `json:"homepage"`
-	Email 		string `json:"email"`
+	Name        uint64 `json:"name"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+	Email       string `json:"email"`
 }
